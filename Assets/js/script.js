9d6476ecf7c49555fa105cd6f16beee956f0d532
Cristobal Marquez-Glynn <cristobalmqz@gmail.com>
2022-04-05 09:13:31 -0700

    JS - added textContent for IP/current location info

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,21 +1,34 @@
 // var userFormEl = document.querySelector('#user-form');
 // var citySearchEl = document.querySelector('#citySearch')
+var regionNameIP = document.querySelector('#regionName');
+var cityIP = document.querySelector('#city');
+var zipCodeIP = document.querySelector('#zipCode');
+var latIP = document.querySelector('#lat');
+var lonIP = document.querySelector('#lon');
+var ISP = document.querySelector('#ISP');
+
 var freeGeoIP = 'https://api.freegeoip.app/json/?apikey=74824920-b48a-11ec-aeb7-87f5f0610281';
 
 fetch(freeGeoIP)
       .then(function (response) {
         if (response.ok) {
-          response.json().then(function (data) {
-          console.log(data);
+            response.json().then(function (data) {
+            console.log(data);
+            
+            regionNameIP.textContent = data.region_name;
+            cityIP.textContent = data.region_name;
+            zipCodeIP.textContent = data.zip_code;
+            latIP.textContent = data.latitude;
+            lonIP.textContent = data.longitude;
+            ISP.textContent = data.ip;
+
           });
         } else {
-          console.log(response.statusText);
-          // alert('Error: ' + response.statusText);
+            console.log(response.statusText);
         }
       })
       .catch(function (error) {
           console.log(error);
-        // alert('Unable to connect to goDaddy.com');
       });
 
 // var formSubmitHandler = function (event) {
